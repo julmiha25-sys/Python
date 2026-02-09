70e99002a26f81250bc1cb4f9fef07eb8a95c181
JuliyaMihaleva <julmiha25@gmail.com>
2026-02-09 15:39:33 +0500

    Add Python_API_Postgres project

---
+++ Python_API_Postgres/generate-sales-data.py
@@ -0,0 +1,30 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta  # Работа со временем, вых дни
+import pandas as pd
+from random import randint # Рандомить список компаний по покупке акций
+import configparser
+
+config=configparser.ConfigParser()
+config.read('config.ini')
+
+COMPANIES=eval(config['Companies']['COMPANIES']) # Компании торгуют на бирже
+
+today=datetime.today()
+# today.weekday() - День недели от 0 до 6 (0-Понедельник)
+# Скприпт запускается на следующий день после торгов
+yesterday=today-timedelta(days=1)
+#if 1<=today.weekday()<=5:
+d={
+        'dt': [yesterday.strftime("%d-%m-%Y")]*len(COMPANIES)*2, # 5 компаний * 2 покупка-продажа
+        'company': COMPANIES*2, # Покупка-продажа
+        'transaction_type': ['bye']*len(COMPANIES)+['sell']*len(COMPANIES),
+        'amount': [randint(0,1000) for _ in range(len(COMPANIES)*2)]
+    }
+df=pd.DataFrame(d)
+df.to_csv("sales-data.csv",index=False)
+
+
+
+
+
